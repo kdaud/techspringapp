b900cce29979a3fd7308b9d0c574995a113af49a
kdaud <dkakumirizii@gmail.com>
2020-05-06 21:10:22 +0300

    codes

---
+++ src/com/pack/softwarecodes/Marriage.java
@@ -0,0 +1,17 @@
+package com.pack.softwarecodes;
+
+
+public class Marriage {
+	
+	int num1;
+	
+	int num2;
+	
+	int result;
+	
+	public void calculate() {
+		result = num1 + num2;
+		
+	}
+	
+}
---
+++ src/com/pack/softwarecodes/Tester.java
@@ -0,0 +1,14 @@
+package com.pack.softwarecodes;
+
+
+public class Tester {
+	
+	public static void main(String[] args) {
+		Marriage obj = new Marriage();
+		obj.num1 = 43;
+		obj.num2 = 38;
+		obj.calculate();
+		System.out.println(obj.result);
+	}
+	
+}
